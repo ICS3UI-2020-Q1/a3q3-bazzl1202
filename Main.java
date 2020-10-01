3f1e6724c8c0aa00b2de3d1688e57fd9036fa462
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-01 15:26:23 +0000

    Now tells you the percentage ranges

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks for the letter of your grade and tells you the percentage range it is in.
+ * @author Liana Bazzarella 
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // get the user letter grade input
+    System.out.println("Please enter a letter grade");
+    String letterGrade = input.nextLine();
+
+    //determine the letter grade percentage range
+    switch(letterGrade){
+      case "A": 
+      System.out.println("This is between 80% and 100%");
+      break;
+      case "B":
+      System.out.println("This is between 70% and 79%");
+      break; 
+      case "C":
+      System.out.println("This is between 60% and 69%");
+      break;
+      case "D":
+      System.out.println("This is between 50% and 59%");
+      break;
+      case "F":
+      System.out.println("This is between 0% and 49%");
+      break;
+      default:
+      System.out.println("Invalid letter grade");
+    }
     
   }
 }
